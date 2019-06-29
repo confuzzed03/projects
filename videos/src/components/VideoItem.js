6b0b2fad72d3d996028c8823f5c8d52f73385738
Andy <confuzzed03@gmail.com>
2019-06-29 11:36:42 -0400

    Adding blog, songs, videos tutorials, starting blog

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,12 +1,17 @@
 import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className='video-item item'>
+    <div
+      className='video-item item'
+      onClick={() => {
+        onVideoSelect(video);
+      }}
+    >
       <img
         className='ui image'
-        alt=''
+        alt={video.snippet.title}
         src={video.snippet.thumbnails.medium.url}
       />
       <div className='content'>
